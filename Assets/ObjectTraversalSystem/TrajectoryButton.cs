e0daa79a6636a479e71a9848bf01a5cceef96e5f
Leonardo Mota <contact@leoism.me>
2021-11-04 21:17:43 -0700

    Add button for Ray

--- Assets/ObjectTraversalSystem/TrajectoryButton.cs
+++ Assets/ObjectTraversalSystem/TrajectoryButton.cs
@@ -7,12 +7,16 @@ public class TrajectoryButton : MonoBehaviour
 {
   public ShootingBehavior cat = null;
   public BoomerangShooter boom = null;
+  public RayShooter ray = null;
+  public GameObject aim = null;
   public Text displayText = null;
   // Start is called before the first frame update
   void Start()
   {
     Debug.Assert(boom != null);
     Debug.Assert(cat != null);
+    Debug.Assert(aim != null);
+    Debug.Assert(ray != null);
   }
 
   public void ActivateBehavior(int idx)
@@ -21,13 +25,25 @@ public class TrajectoryButton : MonoBehaviour
     {
       cat.enabled = true;
       boom.enabled = false;
+      ray.enabled = false;
       displayText.text = "Trajectory For: Arrow, Bomb, Alien Grenade, Bowling Ball, Deforester, Magnet";
+      aim.SetActive(false);
     }
     else if (idx == 1)
     {
       boom.enabled = true;
       cat.enabled = false;
+      ray.enabled = false;
       displayText.text = "Trajectory For: Boomerang";
+      aim.SetActive(false);
+    }
+    else if (idx == 2)
+    {
+      ray.enabled = true;
+      cat.enabled = false;
+      boom.enabled = false;
+      displayText.text = "Trajectory For: Ray";
+      aim.SetActive(true);
     }
   }
 }
