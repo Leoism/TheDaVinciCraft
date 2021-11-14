6e409e89987ff10a8bc1f845adb14931052a55b1
manling168 <manling@uw.edu>
2021-11-14 13:13:16 -0800

    Buying scene,arts and weapon sprites

---
+++ Assets/Scripts/Art/Arts.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Arts
+{
+    public string artName;
+    public Sprite artSprite;
+}
