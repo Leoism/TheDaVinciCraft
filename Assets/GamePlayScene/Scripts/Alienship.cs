9775992cb1c82f043050ce7f76302dacb2a7d98f
sellithy <shehab.ellithy@gmail.com>
2021-11-22 12:18:42 -0800

    Cosmetic

--- Assets/GamePlayScene/Scripts/Alienship.cs
+++ Assets/GamePlayScene/Scripts/Alienship.cs
@@ -2,57 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class Alienship : MonoBehaviour
 {
-  bool isInitialized = false;
-  private float currentTime = 0;
-  public GameObject powerBar = null;
-  public ShootingBehavior shootingBehavior = null;
-  // Start is called before the first frame update
-  void Start()
-  {
-    gameObject.SetActive(false);
-    powerBar.SetActive(false);
-    currentTime = 10;
-  }
-  public void Init()
-  {
-    if (isInitialized) return;
-    powerBar.SetActive(true);
-    gameObject.SetActive(true);
-    Vector3 windowCorner = Camera.main.ViewportToWorldPoint(new Vector3(0.8f, 0.85f, 1));
-    transform.position = windowCorner;
-  }
+    bool isInitialized = false;
+    private float currentTime = 0;
+    public GameObject powerBar = null;
 
-  public void UnInit()
-  {
-    if (isInitialized)
+    public ShootingBehavior shootingBehavior = null;
+
+    // Start is called before the first frame update
+    void Start()
     {
-      gameObject.SetActive(false);
-    }
-  }
-  void Update()
-  {
-    // add timer about 10 sec
-    Debug.Log(GameManager.globalManager.alienInventory.TotalCount());
-    if (GameManager.globalManager.alienInventory.TotalCount() <= 0)
-    {
-      CountTenSec();
-    }
-  }
-  private void CountTenSec()
-  {
-    if (Timer.secRemaining < 10)
-    {
-      currentTime = Timer.secRemaining;
-    }
-    currentTime -= 1 * Time.deltaTime;
-    Debug.Log(currentTime);
-    if (currentTime <= 0)
-    {
-      currentTime = 10;
-      SceneManager.LoadScene("HumanWin");
+        gameObject.SetActive(false);
+        powerBar.SetActive(false);
+        currentTime = 10;
     }
 
-  }
+    public void Init()
+    {
+        if (isInitialized) return;
+        powerBar.SetActive(true);
+        gameObject.SetActive(true);
+        Vector3 windowCorner = Camera.main.ViewportToWorldPoint(new Vector3(0.8f, 0.85f, 1));
+        transform.position = windowCorner;
+    }
+
+    public void UnInit()
+    {
+        if (isInitialized)
+            gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        // add timer about 10 sec
+        if (GameManager.globalManager.alienInventory.TotalCount() <= 0)
+            CountTenSec();
+    }
+
+    private void CountTenSec()
+    {
+        if (Timer.secRemaining < 10)
+            currentTime = Timer.secRemaining;
+
+        currentTime -= 1 * Time.deltaTime;
+        if (currentTime <= 0)
+        {
+            currentTime = 10;
+            SceneManager.LoadScene("HumanWin");
+        }
+    }
 }
--- Assets/Scripts/Inventory/ActionBarBehavior.cs
+++ Assets/Scripts/Inventory/ActionBarBehavior.cs
@@ -5,72 +5,59 @@ using UnityEngine.UI;
 
 public class ActionBarBehavior : MonoBehaviour
 {
-  [SerializeField]
-  private List<GameObject> itemsAvailable = new List<GameObject>();
-  public Gameplay gameplayScene = null;
-  void Start()
-  {
-  }
+    [SerializeField] private List<GameObject> itemsAvailable = new List<GameObject>();
+    public Gameplay gameplayScene = null;
 
-  void Update()
-  {
-  }
-
-  public void Render()
-  {
-    Vector3 nextPos = GetComponent<RectTransform>().anchoredPosition;
-    for (int i = 0; i < itemsAvailable.Count; i++)
+    public void Render()
     {
-      GameObject item = itemsAvailable[i];
+        Vector3 nextPos = GetComponent<RectTransform>().anchoredPosition;
+        for (int i = 0; i < itemsAvailable.Count; i++)
+        {
+            GameObject item = itemsAvailable[i];
             Debug.Log(item.name);
-      item.transform.SetParent(transform);
-      RectTransform itemRt = item.GetComponent<RectTransform>();
-      Button itemButton = item.GetComponent<Button>();
-      itemButton.tag = "Button";
-      itemRt.localScale = new Vector3(1, 1, 1);
-      itemRt.anchoredPosition = nextPos;
-      nextPos = itemRt.anchoredPosition - new Vector2(0, itemRt.rect.height);
-      int tempIdx = i;
-      item.GetComponentInChildren<Text>().text = item.GetComponent<Item>().GetCount().ToString();
+            item.transform.SetParent(transform);
+            RectTransform itemRt = item.GetComponent<RectTransform>();
+            Button itemButton = item.GetComponent<Button>();
+            itemButton.tag = "Button";
+            itemRt.localScale = new Vector3(1, 1, 1);
+            itemRt.anchoredPosition = nextPos;
+            nextPos = itemRt.anchoredPosition - new Vector2(0, itemRt.rect.height);
+            int tempIdx = i;
+            item.GetComponentInChildren<Text>().text = item.GetComponent<Item>().GetCount().ToString();
             Tooltip newTT = item.AddComponent<Tooltip>();
             newTT.setMessage(item.GetComponent<Item>().GetMessage());
-      // anonymous function to update count
-      itemButton.onClick.AddListener(() =>
-      {
-        gameplayScene.SetProjectile(item);
-        // UseItem(tempIdx);
-        // item.GetComponentInChildren<Text>().text = item.GetComponent<Item>().GetCount().ToString();
-      });
+            // anonymous function to update count
+            itemButton.onClick.AddListener(() => { gameplayScene.SetProjectile(item); });
+        }
     }
-  }
 
-  public void Reset()
-  {
-    foreach (GameObject item in itemsAvailable)
+    public void Reset()
     {
-      Destroy(item);
+        foreach (GameObject item in itemsAvailable)
+        {
+            Destroy(item);
+        }
+
+        itemsAvailable = null;
     }
-    itemsAvailable = null;
-  }
 
-  public void AddItem(GameObject newItem)
-  {
-    itemsAvailable.Add(newItem);
+    public void AddItem(GameObject newItem)
+    {
+        itemsAvailable.Add(newItem);
+    }
 
-  }
+    public void SetList(List<GameObject> newList)
+    {
+        itemsAvailable = newList;
+        Render();
+    }
 
-  public void SetList(List<GameObject> newList)
-  {
-    itemsAvailable = newList;
-    Render();
-  }
+    public int UseItem(int itemIdx)
+    {
+        return itemsAvailable[itemIdx].GetComponent<Item>().DecreaseCount();
+    }
 
-  public int UseItem(int itemIdx)
-  {
-    return itemsAvailable[itemIdx].GetComponent<Item>().DecreaseCount();
-  }
-
-  public string SetToolTip(string item)
+    public string SetToolTip(string item)
     {
         Debug.Log(item);
         return item;
