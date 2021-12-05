7c25976872d393ff5e906ac6ffde17b784203e3d
manling168 <manling@uw.edu>
2021-12-05 00:19:18 -0800

    set up the item types that players are able to use in each round

--- Assets/Scripts/BuyMenuItemControl.cs
+++ Assets/Scripts/BuyMenuItemControl.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class BuyMenuItemControl : MonoBehaviour
 {
+    // materials
     public GameObject woodPanel;
     public GameObject fabricPanel;
     public GameObject stonePanel;
     public GameObject glassPanel;
     public GameObject metalPanel;
 
+    // weapons
+    // public GameObject deforPanel;
+    // public GameObject boomPanel;
+    // public GameObject arrowPanel;
+    public GameObject bBallPanel;
+    public GameObject bombPanel;
+    public GameObject mePanel;
+    public GameObject magnetPanel;
+    public GameObject rayPanel;
+    public GameObject grenadePanel;
+
+
+
     private void Awake() 
     {
         if (GameManager.globalManager.GetCurrentRound() == 0) {
             RoundOne();
+        }
+        if (GameManager.globalManager.GetCurrentRound() == 1) {
+            RoundTwo();
         }
     }
     private void RoundOne()
@@ -21,11 +38,22 @@ public class BuyMenuItemControl : MonoBehaviour
         stonePanel.SetActive(false);
         glassPanel.SetActive(false);
         metalPanel.SetActive(false);
+
+        bBallPanel.SetActive(false);
+        bombPanel.SetActive(false);
+        mePanel.SetActive(false);
+        magnetPanel.SetActive(false);
+        rayPanel.SetActive(false);
+        grenadePanel.SetActive(false);
     }
     private void RoundTwo()
     {
         glassPanel.SetActive(false);
         metalPanel.SetActive(false);
+
+        magnetPanel.SetActive(false);
+        rayPanel.SetActive(false);
+        grenadePanel.SetActive(false);
     }
 
 }
