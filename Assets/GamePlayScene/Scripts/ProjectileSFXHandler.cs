8bd8c71908c07b5952d5117ee5a4184186165d57
Leonardo Mota <contact@leoism.me>
2021-12-11 20:09:48 -0800

    Make bombs explode on collision + When a bomb collides with anything, it makes sense to make it explode + Add an explosion effect on collision + Create an RPC to play explosion and explosion sound

---
+++ Assets/GamePlayScene/Scripts/ProjectileSFXHandler.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileSFXHandler : MonoBehaviour
+{
+  public List<AudioClip> audioHandler;
+  private AudioSource audioSource;
+  private bool isPlaying = false;
+  void Start()
+  {
+    audioSource = GetComponent<AudioSource>();
+  }
+
+  public void PlayClipAtIndex(int index)
+  {
+    if (isPlaying) return;
+    audioSource.clip = audioHandler[index];
+    audioSource.Play();
+    isPlaying = true;
+  }
+
+  // Names must start with uppercase letter
+  public void PlayClipByName(string name)
+  {
+    if (isPlaying) return;
+    int index = -1;
+    switch (name)
+    {
+      case "Bomb":
+        index = 0;
+        break;
+    }
+
+    if (index >= 0)
+    {
+      audioSource.clip = audioHandler[index];
+      audioSource.Play();
+      isPlaying = true;
+    }
+  }
+}
