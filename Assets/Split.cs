c441277205d772bfa33085ab48a40dc2da420913
Leonardo Mota <contact@leoism.me>
2021-12-13 14:52:45 -0800

    Add Final Tweaks + Add Bobble To Alienship + Reduce Grapple Hooks to 1 + Fix Collider for Grapple Hook

--- Assets/Split.cs
+++ Assets/Split.cs
@@ -57,194 +57,192 @@ public class Split : MonoBehaviour
         HashSet<Vector3Int> tilesToDestroy = new HashSet<Vector3Int>();
 
         //When hit by weapon projectile
-        if (collision.gameObject.CompareTag("Weapon"))
+        if (!collision.gameObject.CompareTag("Weapon")) return;
+        //Gets the projectile type using the sprite attatched to it
+        string projType = collision.gameObject.GetComponent<SpriteRenderer>().sprite.name.ToString();
+        //Debug.Log(projType);
+        /*
+            * Switch statement which allows material-weapon interaction
+            * In this, it only destroys the weapon if it hits a type it can destroy otherwise it bounces off
+            * and destroys after 3 seconds (waiter subroutine)
+            */
+        switch (projType)
         {
-            //Gets the projectile type using the sprite attatched to it
-            string projType = collision.gameObject.GetComponent<SpriteRenderer>().sprite.name.ToString();
-            //Debug.Log(projType);
-            /*
-             * Switch statement which allows material-weapon interaction
-             * In this, it only destroys the weapon if it hits a type it can destroy otherwise it bounces off
-             * and destroys after 3 seconds (waiter subroutine)
-             */
-            switch (projType)
-            {
-                case ("deforestor"):
-                    if (shatter.name != "WoodTile" && shatter.name != "FabricTile")
-                    {
-                        //StartCoroutine(waiter(collision.gameObject));
-                        return;
-                    }
-                    break;
-                case ("Ball"):
-                    if (shatter.name != "GlassTile" && shatter.name != "StoneTile")
-                    {
-                        //StartCoroutine(waiter(collision.gameObject));
-                        return;
-                    }
-                    break;
-                case ("Arrow"):
-                    if (shatter.name != "FabricTile")
-                    {
-                        //StartCoroutine(waiter(collision.gameObject));
-                        return;
-                    }
-                    break;
-                case ("oregon_man"):
-                    if (shatter.name != "WoodTile" && shatter.name != "FabricTile" && shatter.name != "GlassTile")
-                    {
-                        // StartCoroutine(waiter(collision.gameObject));
-                        return;
-                    }
-                    break;
-                case ("Boomerange"):
-                    if (shatter.name != "GlassTile" && shatter.name != "FabricTile")
-                    {
-                        // StartCoroutine(waiter(collision.gameObject));
-
-                        return;
-                    }
-                    break;
-                case ("MineralExtractor"):
-                    if (shatter.name != "StoneTile")
-                    {
-                        //StartCoroutine(waiter(collision.gameObject));  
-                        return;
-                    }
-                    break;
-                // Ray uses a square sprite
-                case ("Square"):
-                    if (shatter.name == "GlassTile")
-                    {
-                        //StartCoroutine(waiter(collision.gameObject));  
-                        return;
-                    }
-                    break;
-                case "bumb":
-                    if (shatter.name == "MetalTile")
-                    {
-                        return;
-                    }
-                    break;
-                case "GrappleHook":
+            case ("deforestor"):
+                if (shatter.name != "WoodTile" && shatter.name != "FabricTile")
+                {
+                    //StartCoroutine(waiter(collision.gameObject));
                     return;
-            }
-
-            if (projType == "Square" || projType == "MineralExtractor")
-            {
-                GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-                CompletelyShatter();
-                GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-                return;
-            }
-            if (projType == "alien grenade")
-            {
-                GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-                CompletelyDestory();
-                GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-                return;
-            }
-            //Make the rigidbody of the tilemap static so that it does not move on hit
-            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-            //Vector tracking hit position on the tilemap
-            Vector3 hitPosition = Vector3.zero;
-            //Num tile destroyed by hit
-            int tilesDestroyed = 0;
-            //A list containing all the tiles hit in tilemap space
-            List<Vector3Int> hitPos = new List<Vector3Int>();
-            foreach (ContactPoint2D hit in collision.contacts)
-            {
-                //Hit point is offset so that it refers to center of cell rather than edge
-                hitPosition.x = hit.point.x + 2f * hit.normal.x;
-                hitPosition.y = hit.point.y + 2f * hit.normal.y;
-                //Get the tile at the hitpoint
-                t = tl.GetTile<Tile>(tl.WorldToCell(hitPosition));
-                //If there is a tile placed tile there
-                if (tl.GetTile(tl.WorldToCell(hitPosition)) != null)
-                {
-                    //Clear that tile
-                    tl.SetTile(tl.WorldToCell(hitPosition), null);
-                    //Add that tile to the hit tiles list
-                    hitPos.Add(tl.WorldToCell(hitPosition));
-                    tilesToDestroy.Add(tl.WorldToCell(hitPosition));
-                    //Up the num tiles destroyed
-                    tilesDestroyed++;
                 }
-            }
-
-            //Goes through the tiles destroyed and applies shatter
-            for (int i = 0; i <= tilesDestroyed - 1; i++)
-            {
-                Vector3 shattterPos = tl.GetCellCenterWorld(hitPos[i]);
-                //Creates three smaller blocks that have the same sprite as tile destroyed at the hit pos
-                //Offsets each shatter spawn a small amount so that one shatter spawns per quadrant
-                for (int z = 0; z < 4; z++)
+                break;
+            case ("Ball"):
+                if (shatter.name != "GlassTile" && shatter.name != "StoneTile")
                 {
-                    // resets to cell world center
-                    shattterPos = tl.GetCellCenterWorld(hitPos[i]);
-                    switch (z)
-                    {
-                        case 0:
-                            shattterPos.y = shattterPos.y + 0.5f;
-                            shattterPos.x = shattterPos.x - 0.5f;
-                            break;
-                        case 1:
-                            shattterPos.y = shattterPos.y + 0.5f;
-                            shattterPos.x = shattterPos.x + 0.5f;
-                            break;
-                        case 2:
-                            shattterPos.y = shattterPos.y - 0.5f;
-                            shattterPos.x = shattterPos.x - 0.5f;
-                            break;
-                        case 3:
-                            shattterPos.y = shattterPos.y - 0.5f;
-                            shattterPos.x = shattterPos.x + 0.5f;
-                            break;
-                    }
+                    //StartCoroutine(waiter(collision.gameObject));
+                    return;
+                }
+                break;
+            case ("Arrow"):
+                if (shatter.name != "FabricTile")
+                {
+                    //StartCoroutine(waiter(collision.gameObject));
+                    return;
+                }
+                break;
+            case ("oregon_man"):
+                if (shatter.name != "WoodTile" && shatter.name != "FabricTile" && shatter.name != "GlassTile")
+                {
+                    // StartCoroutine(waiter(collision.gameObject));
+                    return;
+                }
+                break;
+            case ("Boomerange"):
+                if (shatter.name != "GlassTile" && shatter.name != "FabricTile")
+                {
+                    // StartCoroutine(waiter(collision.gameObject));
 
-                    GameObject shatterTile = null;
-                    if (GameManager.globalManager.isOnlineMode)
+                    return;
+                }
+                break;
+            case ("MineralExtractor"):
+                if (shatter.name != "StoneTile")
+                {
+                    //StartCoroutine(waiter(collision.gameObject));  
+                    return;
+                }
+                break;
+            // Ray uses a square sprite
+            case ("Square"):
+                if (shatter.name == "GlassTile")
+                {
+                    //StartCoroutine(waiter(collision.gameObject));  
+                    return;
+                }
+                break;
+            case "bumb":
+                if (shatter.name == "MetalTile")
+                {
+                    return;
+                }
+                break;
+            case "GrappleHook":
+                return;
+        }
+
+        if (projType == "Square" || projType == "MineralExtractor")
+        {
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            CompletelyShatter();
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            return;
+        }
+        if (projType == "alien grenade")
+        {
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            CompletelyDestory();
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            return;
+        }
+        //Make the rigidbody of the tilemap static so that it does not move on hit
+        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        //Vector tracking hit position on the tilemap
+        Vector3 hitPosition = Vector3.zero;
+        //Num tile destroyed by hit
+        int tilesDestroyed = 0;
+        //A list containing all the tiles hit in tilemap space
+        List<Vector3Int> hitPos = new List<Vector3Int>();
+        foreach (ContactPoint2D hit in collision.contacts)
+        {
+            //Hit point is offset so that it refers to center of cell rather than edge
+            hitPosition.x = hit.point.x + 2f * hit.normal.x;
+            hitPosition.y = hit.point.y + 2f * hit.normal.y;
+            //Get the tile at the hitpoint
+            t = tl.GetTile<Tile>(tl.WorldToCell(hitPosition));
+            //If there is a tile placed tile there
+            if (tl.GetTile(tl.WorldToCell(hitPosition)) != null)
+            {
+                //Clear that tile
+                tl.SetTile(tl.WorldToCell(hitPosition), null);
+                //Add that tile to the hit tiles list
+                hitPos.Add(tl.WorldToCell(hitPosition));
+                tilesToDestroy.Add(tl.WorldToCell(hitPosition));
+                //Up the num tiles destroyed
+                tilesDestroyed++;
+            }
+        }
+
+        //Goes through the tiles destroyed and applies shatter
+        for (int i = 0; i <= tilesDestroyed - 1; i++)
+        {
+            Vector3 shattterPos = tl.GetCellCenterWorld(hitPos[i]);
+            //Creates three smaller blocks that have the same sprite as tile destroyed at the hit pos
+            //Offsets each shatter spawn a small amount so that one shatter spawns per quadrant
+            for (int z = 0; z < 4; z++)
+            {
+                // resets to cell world center
+                shattterPos = tl.GetCellCenterWorld(hitPos[i]);
+                switch (z)
+                {
+                    case 0:
+                        shattterPos.y = shattterPos.y + 0.5f;
+                        shattterPos.x = shattterPos.x - 0.5f;
+                        break;
+                    case 1:
+                        shattterPos.y = shattterPos.y + 0.5f;
+                        shattterPos.x = shattterPos.x + 0.5f;
+                        break;
+                    case 2:
+                        shattterPos.y = shattterPos.y - 0.5f;
+                        shattterPos.x = shattterPos.x - 0.5f;
+                        break;
+                    case 3:
+                        shattterPos.y = shattterPos.y - 0.5f;
+                        shattterPos.x = shattterPos.x + 0.5f;
+                        break;
+                }
+
+                GameObject shatterTile = null;
+                if (GameManager.globalManager.isOnlineMode)
+                {
+                    shatterTile = PhotonNetwork.Instantiate(tileType, shattterPos, Quaternion.identity, 0, new object[] { gameObject.tag });
+                }
+                else
+                {
+                    shatterTile = Instantiate(shatter, shattterPos, Quaternion.identity) as GameObject;
+                    shatterTile.tag = gameObject.tag;
+                }
+                shatterTile.AddComponent<shatterDestroy>();
+            }
+        }
+        /*
+            * Goes through tilemap, searches for non-continous portions.
+            * When found, it saves the tiles left, destroyes the tilemap, and recreates it as 2 seperate tilemaps
+            */
+        for (int x = tl.cellBounds.min.x; x < tl.cellBounds.max.x && breaking; x++)
+        {
+            for (int y = tl.cellBounds.min.y; y < tl.cellBounds.max.y && breaking; y++)
+            {
+                if (tl.GetTile(new Vector3Int(x, y, 0)) != null)
+                {
+                    if (_addedTiles.Count != 0 &&
+                            _addedTiles.All(pos => ManhattanDistance(oldTilePos, new Vector3Int(x, y, 0)) != 1))
                     {
-                        shatterTile = PhotonNetwork.Instantiate(tileType, shattterPos, Quaternion.identity, 0, new object[] { gameObject.tag });
+                        breaking = false;
+                        foreach (var pos in _addedTiles)
+                        {
+                            tilesToDestroy.Add(pos);
+                            tl.SetTile(pos, null);
+                        }
+                        create = true;
                     }
                     else
                     {
-                        shatterTile = Instantiate(shatter, shattterPos, Quaternion.identity) as GameObject;
-                        shatterTile.tag = gameObject.tag;
-                    }
-                    shatterTile.AddComponent<shatterDestroy>();
-                }
-            }
-            /*
-             * Goes through tilemap, searches for non-continous portions.
-             * When found, it saves the tiles left, destroyes the tilemap, and recreates it as 2 seperate tilemaps
-             */
-            for (int x = tl.cellBounds.min.x; x < tl.cellBounds.max.x && breaking; x++)
-            {
-                for (int y = tl.cellBounds.min.y; y < tl.cellBounds.max.y && breaking; y++)
-                {
-                    if (tl.GetTile(new Vector3Int(x, y, 0)) != null)
-                    {
-                        if (_addedTiles.Count != 0 &&
-                              _addedTiles.All(pos => ManhattanDistance(oldTilePos, new Vector3Int(x, y, 0)) != 1))
-                        {
-                            breaking = false;
-                            foreach (var pos in _addedTiles)
-                            {
-                                tilesToDestroy.Add(pos);
-                                tl.SetTile(pos, null);
-                            }
-                            create = true;
-                        }
-                        else
-                        {
-                            oldTilePos = new Vector3Int(x, y, 0);
-                            _addedTiles.Add(new Vector3Int(x, y, 0));
-                        }
-
+                        oldTilePos = new Vector3Int(x, y, 0);
+                        _addedTiles.Add(new Vector3Int(x, y, 0));
                     }
 
                 }
+
             }
         }
 
